e2c53f697ce1e6dcf85f0adac3bc72b2d9e87f0f
G4BR-13-L <gabriel.victorc13@gmail.com>
2025-08-26 23:00:06 -0300

    fixed delete route

--- src/infrastructure/http/routes.rs
+++ src/infrastructure/http/routes.rs
@@ -4,9 +4,10 @@ use crate::infrastructure::http::handlers::{estatistica_handler, transacao_handl
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/transacoes").route("", web::get().to(transacao_handler::find_all)))
-        .service(web::scope("/transacao").route("", web::post().to(transacao_handler::create)))
         .service(
-            web::scope("/transacao").route("", web::delete().to(transacao_handler::delete_all)),
+            web::scope("/transacao")
+                .route("", web::post().to(transacao_handler::create))
+                .route("", web::delete().to(transacao_handler::delete_all)),
         )
         .service(web::scope("/estatistica").route("", web::get().to(estatistica_handler::hello)));
 }
